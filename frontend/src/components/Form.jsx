c189f940caecde3d72140a57d0cc9c7ca2002c8e
Геннадий <gennadiy.mutalibov@gmail.com>
2025-11-25 18:18:23 +0300

    dfdfdf

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -4,6 +4,7 @@ export default function form() {
   return (
     <div className='container form-container'>
       <form>
+        Привет!
         <input type="text" className='input input-name'/>
         <input type="text" className='input input-cirname'/>
         <button className='btn form-btn'>Отправить</button>
